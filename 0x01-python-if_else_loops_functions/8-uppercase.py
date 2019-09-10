4b17117b343934ed9b65f47bb0a2cae17cd998c4
william <818@holbertonschool.com>
2019-09-10 16:16:47 +0000

    [BUGFIX] Fix wrong format code input

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     low = str + "\n"
     for i in low:
         if ord(i) > 96 and ord(i) < 123:
-            print("{:c}".format(chr(ord(i) - 32)), end="")
+            print("{:c}".format(ord(i) - 32), end="")
         else:
-            print("{:c}".format(i), end="")
+            print("{:c}".format(ord(i)), end="")
