e1645f9d8c8437c2f6f9217489bed711f82ea53a
william <818@holbertonschool.com>
2019-09-30 17:31:45 +0000

    [BUGFIX] Reset zero handling, add IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,10 @@
 
 def safe_print_list(my_list=[], x=0):
     """Print a list with exception handling"""
-    if x == 0:
-        return 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-        except:
+        except IndexError:
             print()
             return i + 1
     print()
