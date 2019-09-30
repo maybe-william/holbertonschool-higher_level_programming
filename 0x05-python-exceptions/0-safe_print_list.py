0acb2e0c97da510626a6b944fe62a9e10b0a47b5
william <818@holbertonschool.com>
2019-09-30 17:33:15 +0000

    [BUGFIX] Fix exception off by one error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         except IndexError:
             print()
-            return i + 1
+            return i
     print()
     return x
