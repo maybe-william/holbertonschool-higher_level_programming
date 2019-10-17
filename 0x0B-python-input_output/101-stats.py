b7b829e7589b698dffdb7e607cdbbc002bab854e
william <818@holbertonschool.com>
2019-10-17 02:54:28 +0000

    [INIT] Do first try

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" module: log parsing """
+import sys
+import signal
+
+
+def signal_handler(sig, frame):
+    printLog()
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+class Dat:
+    """ class to hold data """
+    filesizes = [0]
+    status = {}
+
+
+def printLog():
+    """ Print one log """
+    size = sum(Dat.filesizes)
+    print("File size: {}".format(size))
+    x = sorted(Dat.status.keys())
+    for k in x:
+        print(str(k) + ": " + str(Dat.status[k]))
+    Dat.filesizes = [0]
+    Dat.status = {}
+
+
+count = 0
+for i in sys.stdin:
+    if count == 10:
+        printLog()
+        count = 0
+    fsize = int(i.split(' ')[-1].rstrip())
+    Dat.filesizes.append(fsize)
+    code = int(i.split(' ')[-2])
+    Dat.status[code] = Dat.status.get(code, 0) + 1
+    count = count + 1
