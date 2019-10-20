4e2f72d8f133250ad22a6aa07b5d731f81f3d411
william <818@holbertonschool.com>
2019-10-20 22:31:51 +0000

    [INIT] Start forming tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""module: test_models"""
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """class: TestRectangle"""
+
+    m = "Not implemented"
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up class tests"""
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class tests"""
+        pass
+
+    def test_init(self):
+        """Test init works"""
+        fail(m)
+    
+    def test_nbobjs(self):
+        """Test nb_objs works"""
+        fail(m)
+
+    def test_valint(self):
+        """Test val_int works"""
+        fail(m)
+
+    def test_create(self):
+        """Test create works"""
+        fail(m)
+
+    def test_tojson(self):
+        """Test to_json_string works"""
+        fail(m)
+
+    def test_fromjson(self):
+        """Test from_json_string works"""
+        fail(m)
+
+    def test_save(self):
+        """Test save_to_file works"""
+        fail(m)
+
+    def test_load(self):
+        """Test load_from_file works"""
+        fail(m)
+
+    def test_savecsv(self):
+        """Test save_to_file_csv works"""
+        fail(m)
+
+    def test_loadcsv(self):
+        """Test load_from_file_csv works"""
+        fail(m)
