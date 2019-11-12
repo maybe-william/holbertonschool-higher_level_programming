5a26b6086fc51297cacd182c2bdfa642dc1aef69
william <818@holbertonschool.com>
2019-11-12 14:48:53 +0000

    [BUG] Add failures to test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,36 +9,45 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_empty(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertIs(max_integer([]), None)
 
     def test_end(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([1, 2, 3]), 3)
 
     def test_beginning(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([3, 2, 1]), 3)
 
     def test_middle(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([1, 3, 2]), 3)
 
     def test_negative(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([2, 3, -4]), 3)
 
     def test_all_negative(self):
         """ test empty list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([-2, -3, -4]), -2)
 
     def test_samelist(self):
         """ test same list """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([3, 3, 3]), 3)
 
     def test_oneitem(self):
         """ test a list with only one item """
+        self.assertEqual(1, 0)
         self.assertEqual(max_integer([3]), 3)
 
     def test_noargs(self):
         """ test no list """
+        self.assertEqual(1, 0)
         self.assertIs(max_integer(), None)
