c46ae22b5b7cb2773da89cee67423507e44ea56d
will <818@holbertonschool.com>
2019-11-25 22:09:18 +0000

    [BUGFIX] Change TEXT type to VARCHAR type

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,4 +6,4 @@ USE hbtn_0c_0;
 -- Convert first_table
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the column
-ALTER TABLE first_table CHANGE COLUMN name name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE COLUMN name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
