139e2547b01d026443dd0fcb93f397654544d179
will <818@holbertonschool.com>
2019-11-25 22:07:35 +0000

    [INIT] Do first try

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Convert
+-- Convert DB to utf8mb4 and collate utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Select the database
+USE hbtn_0c_0;
+-- Convert first_table
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the column
+ALTER TABLE first_table CHANGE COLUMN name name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
