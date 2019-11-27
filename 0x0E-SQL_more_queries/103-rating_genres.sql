b4a96d7731925cc0c6509e244b5ccdec73233815
will <818@holbertonschool.com>
2019-11-27 01:35:28 +0000

    [BUGFIX] Uppercase the keyword 'as'

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,3 @@
 -- Count genre ratings
 -- Select query
-SELECT t.name, SUM(r.rate) as rating FROM tv_genres t INNER JOIN tv_show_genres g ON t.id = g.genre_id INNER JOIN tv_shows s ON g.show_id = s.id INNER JOIN tv_show_ratings r ON r.show_id = s.id GROUP BY t.name ORDER BY rating DESC;
+SELECT t.name, SUM(r.rate) AS rating FROM tv_genres t INNER JOIN tv_show_genres g ON t.id = g.genre_id INNER JOIN tv_shows s ON g.show_id = s.id INNER JOIN tv_show_ratings r ON r.show_id = s.id GROUP BY t.name ORDER BY rating DESC;
