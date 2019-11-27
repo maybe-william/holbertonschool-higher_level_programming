ed157a42e5f4fddc66a21e62e7824f9cdc3a240f
will <818@holbertonschool.com>
2019-11-27 01:02:21 +0000

    [INIT] Do first try

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,3 @@
+-- Count Dexter genres
+-- Select query
+SELECT DISTINCT t.name FROM tv_genres t INNER JOIN tv_show_genres g ON t.id = g.genre_id INNER JOIN tv_shows s ON g.show_id = s.id WHERE t.name NOT IN (SELECT t.name FROM tv_genres t INNER JOIN tv_show_genres g ON t.id = g.genre_id INNER JOIN tv_shows s ON g.show_id = s.id WHERE s.title = "Dexter") ORDER BY t.name ASC;
