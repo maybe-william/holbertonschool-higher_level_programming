d12a4dc20620cc595d378431766d81b23410179b
will <818@holbertonschool.com>
2019-12-15 01:38:44 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+# This module lists entries in a table
+
+if __name__ == '__main__':
+    # argv: 1 is username, 2 is pass, 3 is db name
+    sql = MySQLdb.connect(host="localhost", port=3306,
+                          user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = sql.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetch_all()
+    for row in rows:
+        print(row)
+    cur.close()
+    sql.close()
