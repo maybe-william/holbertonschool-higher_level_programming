f49845478f9398c281669a9a19e36680ec381b20
will <818@holbertonschool.com>
2019-12-15 22:05:41 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+""" This module lists entries in a table that match a name sans SQLi"""
+
+if __name__ == '__main__':
+    # argv: 1 is username, 2 is pass, 3 is db name
+    sql = MySQLdb.connect(host="localhost", port=3306,
+                          user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = sql.cursor()
+
+    s = "SELECT * FROM cities ORDER BY id ASC"
+    cur.execute(s)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    sql.close()
