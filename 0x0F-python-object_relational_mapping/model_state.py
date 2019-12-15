839509cac63d687a5da4a98fa20de116b18af1b0
will <818@holbertonschool.com>
2019-12-15 23:33:43 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+""" The first state model """
+Base = declarative_base()
+
+
+class State(Base):
+    __table__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
