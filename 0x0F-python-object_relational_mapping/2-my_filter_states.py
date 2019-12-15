7f2d3469f67abdc608f8b4e686562c3e14a6744c
will <818@holbertonschool.com>
2019-12-15 21:02:27 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+""" This module lists entries in a table that match a name"""
+
+if __name__ == '__main__':
+    # argv: 1 is username, 2 is pass, 3 is db name
+    sql = MySQLdb.connect(host="localhost", port=3306,
+                          user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = sql.cursor()
+    cur.execute("SELECT * FROM states WHERE name IS '{}' ORDER BY id ASC"
+                .format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == argv[4]:
+            print(row)
+    cur.close()
+    sql.close()
