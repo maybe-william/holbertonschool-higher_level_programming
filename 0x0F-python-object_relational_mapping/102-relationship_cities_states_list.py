8a4e531c873aa7de44e53525c4be98efb7940c6c
will <818@holbertonschool.com>
2019-12-16 04:05:26 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from relationship_city import Base, City, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    x = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    engine = create_engine(x, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    q = session.query(City).order_by(City.id)
+    for r in q.all():
+        print(str(r.id) + ": " + str(r.name) + " -> " + str(r.state.name))
