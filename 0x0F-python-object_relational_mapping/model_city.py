0a7ab8c6a8b23f63e04a7699c2b845d3ba8b7719
will <818@holbertonschool.com>
2019-12-16 02:37:54 +0000

    [BUGFIX] Make id unique

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,14 +2,11 @@
 """ The first city model """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-""" The Base variable """
-from model_state import State
+from model_state import Base, State
 
 class City(Base):
     """ The City Model """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(128))
     state_id = Column(Integer, ForeignKey(State.id))
