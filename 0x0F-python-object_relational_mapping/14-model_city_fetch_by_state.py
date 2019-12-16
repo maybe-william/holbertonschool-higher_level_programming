f64116eda071511ac99f6f20b06bf7d21fd2a6f9
will <818@holbertonschool.com>
2019-12-16 02:33:02 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    x = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    engine = create_engine(x, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    q = session.query(City).order_by(City.id)
+    for row in q.all():
+        q2 = session.query(State).filter(State.id == row.state_id)
+        row2 = q2.first()
+        if row2:
+            print(str(row2.name) + ": (" + str(row.id) + ") " + str(row.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" The first city model """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+""" The Base variable """
+from model_state import State
+
+class City(Base):
+    """ The City Model """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey(State.id))
