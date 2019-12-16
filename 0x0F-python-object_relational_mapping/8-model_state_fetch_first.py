b8353e09e04a4175f07eb667ccd4b88b95ed96ca
will <818@holbertonschool.com>
2019-12-16 01:13:19 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    x = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    engine = create_engine(x, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    q = session.query(State).order_by(State.id)
+    row = q.first()
+    if row:
+        print(str(row.id) + ": " + str(row.name))
+    else:
+        print("Nothing")
+    session.close()
