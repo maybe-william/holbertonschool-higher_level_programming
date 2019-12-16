0aff23634271cdb16e834f130bd2e0c460f62d77
will <818@holbertonschool.com>
2019-12-16 02:39:02 +0000

    [BUGFIX] Fix betty

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
+
 class City(Base):
     """ The City Model """
     __tablename__ = 'cities'
