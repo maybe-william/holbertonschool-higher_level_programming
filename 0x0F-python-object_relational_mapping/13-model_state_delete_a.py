b69e7da9d0b907c727225ee4d9919854e022f084
will <818@holbertonschool.com>
2019-12-16 02:08:07 +0000

    [INIT] Do first try

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    x = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    engine = create_engine(x, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    q = session.query(State).filter(State.name.like("%a%"))
+    q.delete(synchronize_session=False)
+    session.commit()
+    session.close()
