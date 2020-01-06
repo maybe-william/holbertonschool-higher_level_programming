e5b7673150794017f9d9f4e18d0d0ca35cc29fc8
will <818@holbertonschool.com>
2020-01-06 23:50:49 +0000

    [BUGFIX] Set things the way they were before

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,9 +11,36 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 1:
         return list_of_integers[0]
 
-    last = False
-    for x in list_of_integers:
-        if last and x < last:
-            return x
-        last = x
-    return last
+    dir1 = False
+    dir2 = False
+    max_peak = False
+    for i in range(len(list_of_integers)):
+        if i == 0:
+            continue
+        else:
+            dir1 = dir2
+            x = list_of_integers[i - 1]
+            y = list_of_integers[i]
+            if x < y:
+                dir2 = 1
+            elif y < x:
+                dir2 = -1
+            else:
+                dir2 = 0
+
+            if i == 1:
+                continue
+
+            if dir1 > 0 and dir2 < 0:
+                if (not max_peak) or (x > max_peak):
+                    max_peak = x
+                    return x
+    if (not max_peak):
+        x = list_of_integers[0]
+        y = list_of_integers[-1]
+        if x > y:
+            max_peak = x
+        else:
+            max_peak = y
+
+    return max_peak
