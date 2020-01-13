a58bbb9518536c322940429abd8f8e03161cad29
will <818@holbertonschool.com>
2020-01-13 19:12:25 +0000

    [BUGFIX] Add default value of None

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 
 if __name__ == "__main__":
     resp = requests.get(sys.argv[1])
-    html = resp.headers["X-Request-Id"]
+    html = resp.headers.get("X-Request-Id", None)
     print(html)
