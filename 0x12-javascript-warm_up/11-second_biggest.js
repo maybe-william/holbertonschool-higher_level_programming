6bc1d528c65f60e06dcb3645d362b8bb05dc3137
will <818@holbertonschool.com>
2020-02-05 00:46:10 +0000

    [BUGFIX] Handle possibility of duplicates in largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 let x = process.argv.slice(2);
 x = x.map(y => parseInt(y));
+x = new Set(x);
+x = Array.from(x);
 x.sort();
 x.reverse();
 if (x.length < 2) {
-  console.log('0');
+  console.log(0);
 } else {
   console.log(x[1]);
 }
