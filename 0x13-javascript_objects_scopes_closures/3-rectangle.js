2a6bce1fbf4409341d841067a70fad66c38ea211
will <818@holbertonschool.com>
2020-02-10 17:54:53 +0000

    [INIT] Do first try

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    var self = this;
+    const x = self.width;
+    let y = self.height;
+    while (y > 0) {
+      console.log('X'.repeat(x));
+      y = y - 1;
+    }
+  }
+}
+module.exports = Rectangle;
